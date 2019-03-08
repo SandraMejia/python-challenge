06dae3769a84ea1db92e4b148b456a77d2cced96
SMA <sandra.mejia.av@gmail.com>
2019-03-08 17:45:28 -0600

    PyParagraph: Request filename from user

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -4,35 +4,45 @@
 
 # ------------------------------------------------------------------------------------
 
+import os
 import re
 
-wordLengthList = []
-sentenceLengthList = []
+wordLengthList = []         # List of the lengths (in letters) of all words
+sentenceLengthList = []     # List of the lengths (in words) of all the sentences
+filename = ''
 
-file = "raw_data/paragraph_2.txt"
+print("Name of file to analyze (located in 'Resources' folder")
+filename = input("Example: paragraph.txt ")
 
-with open(file, 'r') as text:
+try:
+    file = os.path.join('Resources', filename)
+
+
+    with open(file, 'r') as text:
     
-    lines = text.read()
-    print("\n")
-    print("Paragraph to Analyze\n---------------------")
-    print(lines)
-    print("\n")
+        lines = text.read()
+        print("\n")
+        print("Paragraph to Analyze\n---------------------")
+        print(lines)
+        print("\n")
 
-    words = re.split("[ -]", lines)
-    sentences = re.split("(?<=[.!?]) +", lines)
+        words = re.split("[ -]", lines)
+        sentences = re.split("(?<=[.!?]) +", lines)
 
-for word in words:
-    wordLengthList.append(len(word))
-wordlength = round((sum(wordLengthList)/len(wordLengthList)), 2)
+    for word in words:
+        wordLengthList.append(len(word))
+    wordlength = round((sum(wordLengthList)/len(wordLengthList)), 2)
 
-for sentence in sentences:
-    wordsInSentence = re.split("[ -]", sentence)
-    sentenceLengthList.append(len(wordsInSentence))
-sentencelength = round((sum(sentenceLengthList)/len(sentenceLengthList)) , 2)
+    for sentence in sentences:
+        wordsInSentence = re.split("[ -]", sentence)
+        sentenceLengthList.append(len(wordsInSentence))
+    sentencelength = round((sum(sentenceLengthList)/len(sentenceLengthList)) , 2)
 
-print("---------------------\nParagraph Analysis\n---------------------")
-print(f"Approximate Word Count: {len(words)}")
-print(f"Approximate Sentence Count: {len(sentences)}")
-print(f"Average Letter Count: {wordlength}")
-print(f"Average Sentence Length: {sentencelength}\n")
+    print("---------------------\nParagraph Analysis\n---------------------")
+    print(f"Approximate Word Count: {len(words)}")
+    print(f"Approximate Sentence Count: {len(sentences)}")
+    print(f"Average Letter Count: {wordlength}")
+    print(f"Average Sentence Length: {sentencelength}\n")
+
+except:
+    print("\nFile not found")
