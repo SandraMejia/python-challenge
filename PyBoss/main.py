d9cfd26c4a1128cdcfef1badbb5fc4732e8c738d
SMA <sandra.mejia.av@gmail.com>
2019-03-08 16:45:08 -0600

    PyBoss challenge

---
+++ PyBoss/main.py
@@ -0,0 +1,51 @@
+# PyBoss
+
+# Sandra Mejia Avendaño
+
+# ------------------------------------------------------------------------------------
+
+
+import csv
+import os
+
+count = 0            # Counter for number of months
+
+
+employee_csvpath = 'employee_data.csv'
+output_path = 'employee_data_new_format.csv'
+
+us_state_abbrev = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR',
+    'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE',
+    'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+    'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS',
+    'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 'Maryland': 'MD',
+    'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+    'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV',
+    'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'New York': 'NY',
+    'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK',
+    'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC',
+    'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT',
+    'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV',
+    'Wisconsin': 'WI', 'Wyoming': 'WY'}
+
+
+
+with open(employee_csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    with open(output_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerow(["Emp ID","First Name","Last Name","DOB","SSN","State"])
+
+
+    
+        for row in csvreader:
+            name = row[1].split(" ")
+            date = row[2].split("-")
+            date = str(date[1] + "/" + date[2] + "/" + date[0])
+            ssn = row[3].split("-")
+            ssn = str("***-**" + ssn[2])
+            state = us_state_abbrev[row[4]]
+
+            csvwriter.writerow([row[0],name[0],name[1],date,ssn, state])
